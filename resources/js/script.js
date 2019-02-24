437c51994ea6b7ad10e36c79d836b5019ed4abd8
hackerrankAti <43648862+hackerrankAti@users.noreply.github.com>
2019-02-24 11:40:53 +0100

    Add animated effect

--- resources/js/script.js
+++ resources/js/script.js
@@ -61,6 +61,36 @@ $('a[href*="#"]')
     }
   });
     
+    /* Animation on scroll */
+    $('.js--wp-1').waypoint(function(direction) {
+        $('.js--wp-1').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
     
     
+        /* Animation on scroll */
+    $('.js--wp-2').waypoint(function(direction) {
+        $('.js--wp-2').addClass('animated fadeInUp');
+    }, {
+        offset: '50%'
+    });
+    
+    
+        /* Animation on scroll */
+    $('.js--wp-3').waypoint(function(direction) {
+        $('.js--wp-3').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+    
+    
+        /* Animation on scroll */
+    $('.js--wp-4').waypoint(function(direction) {
+        $('.js--wp-4').addClass('animated pulse');
+    }, {
+        offset: '50%'
+    });
+    
+   
 });
